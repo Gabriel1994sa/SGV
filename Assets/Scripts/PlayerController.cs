4949d71d67c32a348cd061ca79ff97bf0adf8ba5
Gab1994 <gabriel.backmann@gmx.de>
2020-05-10 17:19:52 +0200

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Transform _transform;
+    Camera _camera;
+
+    public float speed;
+    public Projectile laserShot;
+
+    private float shotTimer;
+
+    private const float shotDelay = 0.2f;
+
+    void Start()
+    {
+        this._transform = transform;
+        this._camera = Camera.main;
+
+        shotTimer = 0;
+    }
+
+    //Standard UpdateLoop (once per Frame)
+    void Update()
+    {
+        this.Rotate();
+        
+        if (Input.GetKey("w"))
+        {
+            this.MoveUp();
+        }
+        if (Input.GetKey("s"))
+        {
+            this.MoveDown();
+        }
+        if (Input.GetKey("a"))
+        {
+            this.MoveLeft();
+        }
+        if (Input.GetKey("d"))
+        {
+            this.MoveRight();
+        }
+
+        shotTimer -= Time.deltaTime;
+        if (Input.GetButton("Fire1"))
+        {
+            if(shotTimer < 0)
+            {
+                this.FireShot(Input.mousePosition);
+                shotTimer = shotDelay;
+            }          
+        }
+    }
+
+    void Rotate(){
+        Vector2 mousePos = this._camera.ScreenToWorldPoint(Input.mousePosition);
+        float angleRad = Mathf.Atan2(mousePos.y - this._transform.position.y, mousePos.x - this._transform.position.x);
+        float angleDeg = (180 / Mathf.PI) * angleRad;
+        this._transform.rotation = Quaternion.Euler(0, 0, angleDeg - 90);//diese -90 sind nötig für Sprites, die nach oben zeigen. Nutzen Sie andere Assets, könnte es sein, dass die das anpassen müssen       
+    }
+
+    void MoveUp()
+    {
+        this._transform.position += Time.deltaTime * speed * new Vector3(0, 1, 0);
+    }
+
+    void MoveDown()
+    {
+        this._transform.position += Time.deltaTime * speed * new Vector3(0, -1, 0);
+    }
+
+    void MoveLeft()
+    {
+        this._transform.position += Time.deltaTime * speed * new Vector3(-1, 0, 0);
+    }
+
+    void MoveRight()
+    {
+        this._transform.position += Time.deltaTime * speed * new Vector3(1, 0, 0);
+    }
+
+    void FireShot(Vector3 direction)
+    {
+        Instantiate(laserShot, this._transform.position, this._transform.rotation).Init(direction);
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Vector3 _direction;
+    Transform _transform;
+    Camera _camera;
+
+    public float shotSpeed;
+
+    private float lifeTime;
+    private Vector3 velocity;
+
+    private const float maxLifeTime = 10;
+    
+    //We set values in a Init method. Virtual, so we can extend it later :)
+    public virtual Projectile Init(Vector3 direction){
+        this._direction = direction;
+
+        this._direction = Camera.main.ScreenToWorldPoint(direction);
+        this._direction.z = 0;
+
+        return this;
+    }
+
+    void Start()
+    {
+        this._transform = this.transform;
+        this._camera = Camera.main;
+        //this.Rotate();
+        lifeTime = 0;
+        velocity = (this._direction - this._transform.position).normalized;
+    }
+
+    void Update()
+    {
+        this._transform.position += velocity * Time.deltaTime * shotSpeed;
+        lifeTime += Time.deltaTime;
+        if(lifeTime > maxLifeTime)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    
+    void Rotate(){
+        Vector3 pos = this._transform.position + this._direction;
+        float AngleRad = Mathf.Atan2(pos.y - this._transform.position.y, pos.x - this._transform.position.x);
+        float AngleDeg = (180 / Mathf.PI) * AngleRad;
+        this._transform.rotation = Quaternion.Euler(0, 0, AngleDeg - 90);
+    }
+}
